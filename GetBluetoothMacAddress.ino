4fc68b4a65c8372625bb487b31173c2d690256f9
Amitm0v <amit8564ami@gmail.com>
2025-02-03 00:00:21 +0530

    Update GetBluetoothMacAddress.ino

--- GetBluetoothMacAddress.ino
+++ GetBluetoothMacAddress.ino
@@ -5,7 +5,6 @@ void setup()
     Serial.begin(115200);
     Ps3.begin();
     String address = Ps3.getAddress();
-
     Serial.print("The ESP32's Bluetooth MAC address is: "); // to display you mad address
     Serial.println(address);
 }
